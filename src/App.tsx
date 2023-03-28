cd502b43932ded9a758addb717ac6f030c2f1418
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-03-29 00:33:46 +0400

    notify for sample

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,15 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 import { useStore } from 'effector-react'
 import { createEffect, createEvent, createStore } from 'effector'
 
-
-const sayHi = createEffect(async (role: string) => {
-  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  return result.json();
-});
-
-
-sayHi.done.watch((payload: any) => {
-  console.warn(payload);
-});
-
-sayHi.doneData.watch((payload: any) => {
-  console.warn(payload);
-});
-
-
 function App() {
+  // write about sample  
+  return <>
 
-  useEffect(() => {
-    sayHi('admin');
-  }, []);
-
-  return <></>
+  </>
 }
 
 export default App
