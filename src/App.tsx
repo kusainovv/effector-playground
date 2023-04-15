f90e5ce0fd33ee9b3cefc9ebd00833b11b950af3
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-04-16 02:30:51 +0400

    add target field --refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
 import { useStore } from 'effector-react'
-import { createEffect, createEvent, createStore } from 'effector'
+import { createEffect, createEvent, createStore, sample } from 'effector'
+
+const callSample = createEffect();
+
+const updateStore = createEffect();
+const store = createStore(Math.random())
+  .on(updateStore, () => {})
 
 function App() {
-  // write about sample  
-  return <>
-
-  </>
+  return <button onClick={callSample}>
+    call sample
+  </button>
 }
+
+sample({
+  clock: callSample,
+  source: store,
+  target: callSample,
+  fn: (payload: any) => {
+    console.warn(payload);
+  }
+});
 
 export default App
